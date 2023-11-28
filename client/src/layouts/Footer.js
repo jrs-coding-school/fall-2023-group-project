59cdf687f1a18a31bc58bc589db2f9e0c4269163
Evan <you@example.com>
2023-11-28 15:15:36 -0500

    Co-authored-by: Amin-JRS <Amin-JRS@users.noreply.github.com> Co-authored-by: Zane-L-B-O <Zane-L-B-O@users.noreply.github.com> Co-authored-by: Anthony-H-K <Anthony-H-K@users.noreply.github.com>

--- client/src/layouts/Footer.js
+++ client/src/layouts/Footer.js
@@ -1,11 +1,13 @@
-import { Box } from '@mui/material'
+import { Box } from "@mui/material";
 
-function Footer (props) {
+function Footer(props) {
   return (
     <Box>
-      Footer
+      © 2023 Evan Did All This. All rights reserved. Crafted with a heap of ❤️
+      and modesty by the one and only Evan because, let's face it, who else
+      could pull this off?
     </Box>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
