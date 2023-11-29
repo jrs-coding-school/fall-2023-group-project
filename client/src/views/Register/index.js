9c074d66970125b8e4a9e0bc1795c63be78e2302
Evan <you@example.com>
2023-11-29 13:21:00 -0500

    fixed error about roles, added styling

--- client/src/views/Register/index.js
+++ client/src/views/Register/index.js
@@ -1,16 +1,17 @@
-//** Import Statements
 import { useState } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-import { Container, Grid, Typography, Button } from "@mui/material";
-
-//** Setup (define helper functions and variables here)
+import { Container, Grid, Typography, Button, Snackbar } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { register } from "../../utility/api";
 
 function Register(props) {
+  const navigate = useNavigate(); // navigate to homepage
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [isRegistered, setIsRegistered] = useState(false);
+  const [openSuccessSnackbar, setOpenSuccessSnackbar] = useState(false); // need setstate for Success Snackbar
+  const [openErrorSnackbar, setOpenErrorSnackbar] = useState(false); // need state for Error Snackbar
 
   const handleClick = async () => {
     // collect form input data
@@ -19,29 +20,29 @@ function Register(props) {
       email,
       password,
     };
-    console.log("formData:", formData);
-    // send to the api to create an account
+
     try {
-      const baseUrl = process.env.REACT_APP_API_URL || "http://localhost:9000";
-      const response = await fetch(`${baseUrl}/users/register`, {
-        method: "POST",
-        body: JSON.stringify(formData),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      if (!response.ok) {
-        throw new Error("Failed to register");
-      }
-      const data = await response.json();
+      // send to the API to create an account
+      const data = await register(formData);
       console.log("registration successful!", data);
-      setIsRegistered(true);
+      setOpenSuccessSnackbar(true); // set success snackbar open on successful registration
+      setTimeout(() => {
+        navigate("/login"); // redirect after showing Success Snackbar
+      }, 2000); // redirect after 2 seconds
     } catch (error) {
-      console.error(error);
+      console.error("Failed to register:", error);
+      setOpenErrorSnackbar(true); // set error snackbar open on failed registration
     }
   };
 
-  //** Return JSX
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpenSuccessSnackbar(false);
+    setOpenErrorSnackbar(false);
+  };
+
   return (
     <Container maxWidth={"xs"}>
       <Typography variant="h3">Registration Page</Typography>
@@ -78,11 +79,36 @@ function Register(props) {
           onChange={(e) => setPassword(e.target.value)}
         />
         {/* register button */}
-        <Button variant="contained" onClick={handleClick} Link>
+        <Button variant="contained" onClick={handleClick}>
           Register
         </Button>
+        {/* success Snackbar */}
+        <Snackbar
+          open={openSuccessSnackbar}
+          autoHideDuration={3000}
+          onClose={handleClose}
+          message="Registration successful!"
+          anchorOrigin={{
+            //appear on center
+            vertical: "top",
+            horizontal: "center",
+          }}
+        />
+        {/* error Snackbar */}
+        <Snackbar
+          open={openErrorSnackbar}
+          autoHideDuration={3000}
+          onClose={handleClose}
+          message="Registration failed! Please try again."
+          anchorOrigin={{
+            //appear on center
+            vertical: "top",
+            horizontal: "center",
+          }}
+        />
       </Grid>
     </Container>
   );
 }
+
 export default Register;
